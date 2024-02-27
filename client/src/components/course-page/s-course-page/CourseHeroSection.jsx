b3e3940987cae2a5b3e80b520f29d3d9a05b7959
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-02-27 12:45:15 +0530

    Add newly created files

---
+++ client/src/components/course-page/s-course-page/CourseHeroSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './CoursePage.css';
+
+export default function CourseHeroSection(){
+
+
+    return(
+
+        <main>
+            <div className="hero-section">
+                <div className="hero-section-container">
+
+                  <div className="hero-main">
+                    
+                  </div>
+
+                  <div className="hero-sub"></div>
+
+            </div>
+            </div>
+        </main>
+    )
+}
