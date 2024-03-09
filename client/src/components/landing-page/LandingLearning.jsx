eba551e7b76dea095424f11041feecebdca57744
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-03-09 14:34:22 +0530

    msg

--- client/src/components/landing-page/LandingLearning.jsx
+++ client/src/components/landing-page/LandingLearning.jsx
@@ -15,11 +15,13 @@ export default function LandingLearning(){
                             Connect with mentors personally and have unlimited doubt clearing sessions. Each session will be curated to help you learn better.</p>
                     </div>
                     <div className="landing-learning-right">
+                        <div className="landing-learning-right-one">
                         <li> <span className="learning-right-icon"></span>Personalized Curriculam</li>
                         <li><span className="learning-right-icon"></span>Unlimited Doubt Clearing Session</li>
                         <li><span className="learning-right-icon"></span>Placement Training</li>
                         <li><span className="learning-right-icon"></span>Impressive CV & Cover Letters</li>
                         <li><span className="learning-right-icon"></span>Post Hiring Support</li>
+                        </div>
                     </div>
                 </div>
             </div>
