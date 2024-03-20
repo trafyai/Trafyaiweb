4ab264a232d67ec130b120dde32deaa4a7947773
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-03-20 16:57:12 +0530

    msg

--- client/src/pages/error-page/errorPage.js
+++ client/src/pages/error-page/errorPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ErrorPage from "../../components/error-page/ErrorPage";
+import { Helmet } from 'react-helmet';
 
 export default function errorPage(){
     return(
+        <>
+        <Helmet>
+        <title>Error 404 (Not Found) </title>
+        </Helmet>
         <ErrorPage/>
+        </>
     )
 }
